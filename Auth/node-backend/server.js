195d700b123003d0a4191d3af37ff0546478c75f
prudentelias1024 <119433507+prudentelias1024@users.noreply.github.com>
2026-09-07 20:04:32 +0100

    server updated

--- Auth/node-backend/server.js
+++ Auth/node-backend/server.js
@@ -38,7 +38,7 @@ if (process.env.NODE_ENV == 'production') {
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors({
-    origin: URL,
+    origin: "*",
     credentials: true,
     
 }))
